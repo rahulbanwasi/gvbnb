1f01210fbe3f1968c7372b9833931a16565cbc97
rahulbanwasi <117411699+rahulbanwasi@users.noreply.github.com>
2023-06-14 14:51:35 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -12,7 +12,7 @@ class CMD(object):
     HELP = ["help", f"help@{bot}"]
     # Open Settings Panel
     SETTINGS = ["settings", f"settings@{bot}"]
-    DOWNLOAD = ["dl", f"dl@{bot}"]
+    DOWNLOAD = ["dl3", f"dl3@{bot}"]
     # Auth user or chat to use the bot
     # TODO Add cmd to remove auth
     AUTH = ["auth", f"auth@{bot}"]
